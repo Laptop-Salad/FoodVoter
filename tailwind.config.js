c52236c4aa6aa4e69fbbfadfc1b3ea9663ffa6af
Amanda <squaretacos456@gmail.com>
2021-11-29 18:45:01 +0000

    updated landing

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': ['Roboto', 'sans-serif']
+            },
+            colors: {
+                darkCyan: '#042F39',
+                lightCyan: '#075264'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
